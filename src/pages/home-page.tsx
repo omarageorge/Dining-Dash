88c22ada8eadca91a081e85174ae354f8b97c162
George Omara <georgeomara8@gmail.com>
2023-06-07 13:54:31 +0300

    Added the RestaurantCard component

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,12 +1,53 @@
+import RestaurantCard from '../components/RestaurantCard';
+
 const HomePage: React.FC = () => {
+  const restaurants = [
+    {
+      name: 'Restaurant 1',
+      location: 'Wakiso',
+      imageSrc: '/images/header-background.jpg',
+      cuisine: 'Italian',
+    },
+    {
+      name: 'Restaurant 2',
+      location: 'Kampala',
+      imageSrc: '/images/header-background.jpg',
+      cuisine: 'Mexican',
+    },
+    {
+      name: 'Restaurant 3',
+      location: 'Gulu',
+      imageSrc: '/images/header-background.jpg',
+      cuisine: 'Chinese',
+    },
+    {
+      name: 'Restaurant 4',
+      location: 'Entebbe',
+      imageSrc: '/images/header-background.jpg',
+      cuisine: 'Indian',
+    },
+    {
+      name: 'Restaurant 5',
+      location: 'Jinja',
+      imageSrc: '/images/header-background.jpg',
+      cuisine: 'Thai',
+    },
+    {
+      name: 'Restaurant 6',
+      location: 'Mbarara',
+      imageSrc: '/images/header-background.jpg',
+      cuisine: 'American',
+    },
+  ];
+
   return (
     <>
       {/* Header */}
-      <header className='relative w-full h-[80vh]'>
+      <header className='relative w-full h-[90vh]'>
         <div className='absolute z-10 w-full h-full bg-[url("/images/header-background.jpg")] bg-top bg-cover'></div>
         <div className='absolute z-10 w-full h-full bg-black opacity-60'></div>
         <div className='absolute z-30 w-full h-full flex  items-center justify-center'>
-          <div className='text-center space-y-4'>
+          <div className='text-center space-y-6'>
             <h1 className='text-4xl italic space-x-1'>
               <span className='text-orange-400'>Dining</span>
               <span className='text-yellow-400'>Dash</span>
@@ -16,7 +57,7 @@ const HomePage: React.FC = () => {
               <input
                 type='search'
                 placeholder='Search by cuisine, location, or restaurant name...'
-                className='w-80 bg-gray-100 px-4 py-2 rounded-sm outline-none text-green placeholder:text-md placeholder:text-slate-500 placeholder:italic md:w-[40rem]'
+                className='w-72 bg-gray-100 px-4 py-2 rounded-sm outline-none text-green placeholder:text-md placeholder:text-slate-500 placeholder:italic md:w-[40rem]'
               />
             </form>
 
@@ -33,6 +74,14 @@ const HomePage: React.FC = () => {
         <h1 className='text-2xl text-slate-800 text-center font-semibold'>
           Featured Restaurants
         </h1>
+
+        <div className='w-full m-auto mt-8 px-4'>
+          <div className='w-full grid grid-cols-1 justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4'>
+            {restaurants.map((restaurant, index) => (
+              <RestaurantCard key={index} restaurant={restaurant} />
+            ))}
+          </div>
+        </div>
       </main>
 
       {/* Footer section */}
