b89a4561512a2ed785e4667b2f2dc189c57b710b
George Omara <georgeomara8@gmail.com>
2023-06-07 18:41:37 +0300

    Extracted Footer to its own component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className='w-full py-6 bg-slate-800 flex items-center justify-center'>
+      <span className='text-gray-200 text-sm text-center md:text-md'>
+        Copyright &copy; 2023 DiningDash. All rights reserved.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
