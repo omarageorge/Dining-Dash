52d25ebee855dcdb0d561b1d97845fe359832f8a
George Omara <georgeomara8@gmail.com>
2023-06-07 19:12:32 +0300

    Created Routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import HomePage from './pages/home-page';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './assets/styles/globals.scss';
+import RestaurantPage from './pages/restaurant-page';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/restaurant',
+    element: <RestaurantPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
