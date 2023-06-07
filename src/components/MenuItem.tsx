cc1690c02d2df079e2b4f4d1018eeb3a08ad6457
George Omara <georgeomara8@gmail.com>
2023-06-07 18:54:45 +0300

    Added more styling

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -14,8 +14,14 @@ const MenuItem: React.FC<MenuItemProps> = ({ dish }) => {
     <div className='w-full bg-gray-100 rounded-sm shadow-sm p-4 sm:w-[80%] md:w-96'>
       <h2 className='text-xl font-semibold'>{dish.name}</h2>
       <p className='text-gray-600 mb-2'>{dish.description}</p>
-      <p className='text-gray-700 mb-2'>Ingredients: {dish.ingredients}</p>
-      <p className='text-gray-700'>Price: {dish.price} UGX</p>
+      <p>
+        <span className='text-gray-800 font-semibold'>Ingredients:</span>
+        <span className='text-gray-700 ml-1'>{dish.ingredients}</span>
+      </p>
+      <p>
+        <span className='text-gray-800 font-semibold'>Price:</span>
+        <span className='text-gray-700 ml-1'>{dish.price} UGX</span>
+      </p>
     </div>
   );
 };
