978e89f9277fca1e518b8c3e64c641998278b1c1
George Omara <georgeomara8@gmail.com>
2023-06-07 19:15:59 +0300

    Wrapped component inside an anchor tag

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -13,27 +13,29 @@ type RestaurantCardProps = {
 
 const RestaurantCard: React.FC<RestaurantCardProps> = ({ restaurant }) => {
   return (
-    <div className='rounded-md overflow-hidden shadow-md'>
-      <img
-        src={restaurant.imageSrc}
-        alt={restaurant.name}
-        className='w-full h-56 object-cover'
-      />
-      <div className='bg-whites text-gray-800 p-4'>
-        <span className='font-semibold '>{restaurant.name}</span>
+    <a href='/restaurant'>
+      <div className='rounded-md overflow-hidden shadow-md'>
+        <img
+          src={restaurant.imageSrc}
+          alt={restaurant.name}
+          className='w-full h-56 object-cover'
+        />
+        <div className='bg-whites text-gray-800 p-4'>
+          <span className='font-semibold '>{restaurant.name}</span>
 
-        <div className='w-full h-auto flex items-center justify-between mt-1 text-sm '>
-          <span className='flex items-center'>
-            <FaConciergeBell color='orange' />
-            <span className='ml-1'>{restaurant.cuisine}</span>
-          </span>
-          <span className='flex items-center'>
-            <FaMapMarkerAlt color='red' />
-            <span className='ml-1'>{restaurant.location}</span>
-          </span>
+          <div className='w-full h-auto flex items-center justify-between mt-1 text-sm '>
+            <span className='flex items-center'>
+              <FaConciergeBell color='orange' />
+              <span className='ml-1'>{restaurant.cuisine}</span>
+            </span>
+            <span className='flex items-center'>
+              <FaMapMarkerAlt color='red' />
+              <span className='ml-1'>{restaurant.location}</span>
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </a>
   );
 };
 
