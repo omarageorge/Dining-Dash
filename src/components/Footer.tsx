43ea321e92aa445296b5a99535d93078c6d3ebe0
George Omara <georgeomara8@gmail.com>
2023-06-08 11:13:18 +0300

    Added type safety to year variable

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 const Footer: React.FC = () => {
-  const year = new Date().getFullYear();
+  const year: number = new Date().getFullYear();
+
   return (
     <footer className='w-full py-6 bg-slate-800 flex items-center justify-center'>
       <span className='text-gray-200 text-sm text-center md:text-md'>
