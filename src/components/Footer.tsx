da666310646fbb0f747e94a83a9c0fe59bd068af
George Omara <georgeomara8@gmail.com>
2023-06-08 11:12:33 +0300

    Made year dynamic

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 const Footer: React.FC = () => {
+  const year = new Date().getFullYear();
   return (
     <footer className='w-full py-6 bg-slate-800 flex items-center justify-center'>
       <span className='text-gray-200 text-sm text-center md:text-md'>
-        Copyright &copy; 2023 DiningDash. All rights reserved.
+        Copyright &copy; {year} DiningDash. All rights reserved.
       </span>
     </footer>
   );
