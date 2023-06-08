c4de57d293ba86393c71c8cda714e66a8d2066ed
George Omara <georgeomara8@gmail.com>
2023-06-08 12:34:29 +0300

    Logo Component

---
+++ src/components/DiningDashLogo.tsx
@@ -0,0 +1,8 @@
+const DiningDashLogo: React.FC = () => (
+  <span className='font-semibold text-xl italic'>
+    <span className='text-orange-500'>Dining</span>
+    <span className='text-yellow-500'>Dash</span>
+  </span>
+);
+
+export default DiningDashLogo;
