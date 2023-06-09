37b9e58a1716ba4f18c9afc33afc02b452fc8ffa
George Omara <georgeomara8@gmail.com>
2023-06-09 10:55:05 +0300

    Converted app router to an independent component

---
+++ src/routes/routes.tsx
@@ -0,0 +1,62 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ProtectedRoute from '../components/ProtectedRoute';
+import RestaurantPage from '../pages/restaurant-page';
+import ErrorPage from '../pages/error-page';
+import AdminRoot from '../pages/Admin';
+import HomePage from '../pages/home-page';
+import RestaurantsList from '../pages/Admin/restaurants-list';
+import RestaurantAddForm from '../pages/Admin/restaurant-add-form';
+import RestaurantEditForm from '../pages/Admin/restaurant-edit-form';
+import RestaurantMenu from '../pages/Admin/restaurant-menu';
+import RestaurantAddDish from '../pages/Admin/restaurant-add-dish';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: 'restaurant/:restaurantId',
+    element: <RestaurantPage />,
+  },
+  {
+    path: '/admin',
+    element: <ProtectedRoute component={AdminRoot} />,
+    children: [
+      {
+        path: '',
+        element: <RestaurantsList />,
+      },
+      {
+        path: 'restaurants',
+        element: <RestaurantsList />,
+      },
+      {
+        path: 'restaurants/menu',
+        element: <RestaurantMenu />,
+      },
+      {
+        path: 'restaurants/menu/add',
+        element: <RestaurantAddDish />,
+      },
+      {
+        path: 'restaurants/add',
+        element: <RestaurantAddForm />,
+      },
+      {
+        path: 'restaurants/edit',
+        element: <RestaurantEditForm />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <ErrorPage />,
+  },
+]);
+
+const AppRoutes: React.FC = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default AppRoutes;
