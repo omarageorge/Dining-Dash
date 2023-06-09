a7a7bac228a5180529379c5c9543fef4d920a552
George Omara <georgeomara8@gmail.com>
2023-06-09 10:56:08 +0300

    Inserted AppRoute component

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HomePage from './pages/home-page';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import RestaurantPage from './pages/restaurant-page';
-import ErrorPage from './pages/error-page';
+import AppRoutes from './routes/routes';
 import './styles/globals.scss';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: 'restaurant/:restaurantId',
-    element: <RestaurantPage />,
-  },
-  {
-    path: '*',
-    element: <ErrorPage />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AppRoutes />
   </React.StrictMode>
 );
