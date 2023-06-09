d21d4bf499a48e379cec81bb677cb3eb1767184e
George Omara <georgeomara8@gmail.com>
2023-06-09 12:19:15 +0300

    Increased logo size

--- src/components/DiningDashLogo.tsx
+++ src/components/DiningDashLogo.tsx
@@ -1,5 +1,5 @@
 const DiningDashLogo: React.FC = () => (
-  <span className='font-semibold text-xl italic select-none'>
+  <span className='font-semibold text-2xl italic select-none'>
     <span className='text-orange-500'>Dining</span>
     <span className='text-yellow-500'>Dash</span>
   </span>
